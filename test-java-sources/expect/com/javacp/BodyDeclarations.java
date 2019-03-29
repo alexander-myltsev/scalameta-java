b9c7be15c25c0dc8e75fb93c9952f920c6164d5d
Alexander Myltsev <alexander@myltsev.com>
2019-03-29 18:04:41 +0300

    Add body declarations

---
+++ test-java-sources/expect/com/javacp/BodyDeclarations.java
@@ -0,0 +1,9 @@
+package com.javacp;
+
+class BodyDeclarations/*<=com.javacp.BodyDeclarations#*//*<=com.javacp.BodyDeclarations#`<init>`().*/ {
+    String/*=>java.lang.String*/ method/*<=com.javacp.BodyDeclarations#method().*/(Integer/*=>java.lang.Integer*/ a/*<=com.javacp.BodyDeclarations#method().(a)*/) {
+        Integer b/*<=com.javacp.BodyDeclarations#method().b.*/ = a * a;
+        String result/*<=com.javacp.BodyDeclarations#method().result.*/ = b.toString();
+        return result;
+    }
+}
---
+++ test-java-sources/input/com/javacp/BodyDeclarations.java
@@ -0,0 +1,9 @@
+package com.javacp;
+
+class BodyDeclarations {
+    String method(Integer a) {
+        Integer b = a * a;
+        String result = b.toString();
+        return result;
+    }
+}
